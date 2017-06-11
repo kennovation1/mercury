38bc4117512e506a18bf5ab6f69444c55bfda721
kennovation1 <kenrobbins@ieee.org>
2017-06-11 11:40:48 -0400

    Added telelight comands to testui.py

--- testui.py
+++ testui.py
@@ -10,6 +10,7 @@ import pacled64
 import logging
 import time
 import random
+import string
 
 Intensity = 255
 BoardList = [1]
@@ -18,10 +19,15 @@ BoardList = [1]
 def printHelp():
     print '''
       Commands:
-        on <pinlist>       Turn on all pins in the comma separated list (no spaces). All on if no args.
+        on <pinlist>       Turn on all pins in the comma-separated list (no spaces). All on if no args.
         off <pinlist>      Turn of pins in list. All off if no args.
         even               Turn on even pins
         odd                Turn on odd pins
+
+        t <telelight>      Turn on comma-separated list of telelights (no spaces). All on if no args.
+        T <telelight>      Turn off comma-separated list of telelights (no spaces). All off if no args.
+                           Use 'l' or 'r' suffix for left and right respectively.
+                           Telelights are numbered from 1 to N.
 
         chase <boardlist>  Rotate lamps in the specified boards in board list order. 1,2,3,4 if no args.
         rand <boardlist>   Random pattern in the specified boards in board. All boards if no args.
@@ -61,6 +67,10 @@ def processUserInput():
         elif command == 'intensity' or command == 'i':
             Intensity = int(args[0])
             print 'Intensity now: ' + str(Intensity)
+        elif command == 'telelight' or command == 't':
+            handleTelelightCommand(board, args, True)
+        elif command == 'Telelight' or command == 'T':
+            handleTelelightCommand(board, args, True)
         elif command == 'rate' or command == 'r':
             rate = int(args[0])
             print 'Flash rate now: ' + str(rate)
@@ -89,6 +99,40 @@ def handleOnOffCommand(board, args, state):
             pl.setLEDPattern('ALL_ON', Intensity, board=1)
         else:
             pl.setLEDPattern('ALL_OFF', Intensity, board=1)
+
+def handleTelelightCommand(board, args, state):
+    if args and len(args) > 0:
+        for telelight in args:
+            pins = mapTelelightToPins(telelight)
+            for pin in pins:
+                pl.setLEDIntensity(pin, Intensity, board=1)
+    else:
+        if state:
+            pl.setLEDPattern('ALL_ON', Intensity, board=1)
+        else:
+            pl.setLEDPattern('ALL_OFF', Intensity, board=1)
+
+def mapTelelightToPins(telelight):
+    left =  [ 31, 32, 33, 34, 35, 36, 37, 38, 39 ]
+    right = [ 46, 47, 48, 49, 50, 51, 52, 53, 54 ]
+
+    mode = 'b' # Both
+    if string.find(telelight, 'l') >= 0:
+        mode = 'l'
+    elif string.find(telelight, 'r') >= 0:
+        mode = 'r'
+
+    num = int(string.rstrip(telelight, 'lr')) - 1
+
+    pins = []
+
+    if mode in ('b', 'l'):
+        pins.append(left[num])
+
+    if mode in ('b', 'r'):
+        pins.append(right[num])
+
+    return pins
 
 def handleRandom(args):
     lastPin = 1
