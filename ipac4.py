9a167e737ec8f5248c242aaae1cc49c27b58610b
kennovation1 <kenrobbins@ieee.org>
2017-06-18 23:40:24 -0400

    ipac4.py works for switches and telelights

--- ipac4.py
+++ ipac4.py
@@ -2,18 +2,67 @@ from evdev import InputDevice, categorize, ecodes
 dev = InputDevice('/dev/input/event1')
 
 import evdev
-'''
-key_up = 0
-key_down = 1
-key_hold = 2
-print evdev.events.KeyEvent.key_up
-'''
+import pacled64
+
+def mapTelelightToPins(telelight):
+    # As viewed from front, top to bottom, main panel
+    mapping = [ (1,16), (2,17), (3,18), (19,4), (20,5), (21,6), (22,7), (23,8), (9,24) ]
+    return mapping[telelight-1]
+
+def mapSwitchCodeToTelelight(switchCode):
+    switchMap = {
+            36:1,
+            23:2,
+            37:3,
+            97:4,
+            54:5,
+            28:6,
+            24:7,
+            63:8,
+            64:9
+            }
+    return switchMap.get(switchCode, 0)
+
+def handleEvent(key, state):
+    '''
+    key is a byte that represents which which generated the event
+    state is 0 for up, 1, for down, 2 for hold
+    '''
+    states = ['UP', 'DOWN', 'HOLD']
+    print 'Key=' + str(key) + ' State=' + states[state]
+    '''
+    Top switch: 31, left=down, right=up
+    CABIN PRESS: 4, left=down, right=up
+    '''
+    if key == 38:
+        if state == 0:
+            pl.setLEDPattern('ALL_ON', 255)
+        elif state == 1:
+            pl.setLEDPattern('ALL_OFF', 0, )
+    else:
+        telelight = mapSwitchCodeToTelelight(key)
+        print 'TELELIGHT:', telelight
+        if telelight > 0:
+            pins = mapTelelightToPins(telelight)
+            for pin in pins:
+                if state == 0:
+                    pl.setLEDIntensity(pin, 255)
+                elif state == 1:
+                    pl.setLEDIntensity(pin, 0)
+
+
+# Main
+pl = pacled64.PacLED(dryRun=False)
+pl.initializeAllPacLEDs()
 
 for event in dev.read_loop():
     if event.type == ecodes.EV_KEY:
-        print event
         e = categorize(event)
+        '''
+        print event
         print e
         print e.keycode
         print e.keystate
         print e.scancode
+        '''
+        handleEvent(e.scancode, e.keystate)
