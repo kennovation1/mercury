c06e2d568546abe35f54fc3d6da75f7b7a47cb4b
kennovation1 <kenrobbins@ieee.org>
2018-08-11 10:49:19 -0400

    Created scripts to run from init.d

--- src/mercury_run.sh
+++ src/mercury_run.sh
@@ -1,6 +1,7 @@
-cd lightOutputs/
-nohup python lightcontroller.py &
-cd ../switchInputs/
-nohup python readswitches.py -f &
-cd ../controllers/
-nohup python funbox.py &
+# Run the processes to control lights and read switches
+# Each listens to and writes to named piped (FIFOs)
+# Then run controller process (funbox) that reads switches and controls lights
+# by reading and writing the respective FIFOs
+python /home/pi/mercury/src/lightOutputs/lightcontroller.py &
+python /home/pi/mercury/src/switchInputs/readswitches.py -f &
+python /home/pi/mercury/src/controllers/funbox.py &
