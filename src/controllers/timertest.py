55d1a16f4567eba333220c10784a19fde7203f58
kennovation1 <kenrobbins@ieee.org>
2020-07-05 11:17:00 -0400

    Added threading to timertest.py and renamed file

--- src/controllers/timertest.py
+++ src/controllers/timertest.py
@@ -2,7 +2,9 @@
 
 # Environment: Python 2.7
   
-import sched, time
+import sched
+import time
+from threading import Timer
 
 
 s = sched.scheduler(time.time, time.sleep)
@@ -12,6 +14,7 @@ def setLed(led, state):
     print '{}: Setting LED {} {}'.format(time.time(), led, state)
 
 
+'''
 def replayMission():
     eventTime = 1  # Event time in seconds from now
     s.enter(eventTime, 1, setLed, (123, 'ON'))
@@ -20,6 +23,18 @@ def replayMission():
     s.enter(eventTime, 1, setLed, (123, 'OFF'))
     s.enter(eventTime, 1, setLed, (456, 'ON'))
     s.run()
+'''
+def replayMission():
+    eventTime = 1  # Event time in seconds from now
+
+    Timer(eventTime, setLed, (123, 'ON')).start()
+
+    eventTime += 0.3
+    Timer(eventTime, setLed, (123, 'OFF')).start()
+    Timer(eventTime, setLed, (456, 'ON')).start()
 
 
 replayMission()
+while True:
+    print '.'
+    time.sleep(0.25)
